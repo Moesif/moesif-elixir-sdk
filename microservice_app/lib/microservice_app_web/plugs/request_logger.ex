c7d4452c04e66c6f8cef2ec0d01105f4a894a389
Brian Kennedy <brian@moesif.com>
2024-01-03 19:40:20 -0800

    Fix missing context storage of request data

--- microservice_app/lib/microservice_app_web/plugs/request_logger.ex
+++ microservice_app/lib/microservice_app_web/plugs/request_logger.ex
@@ -17,34 +17,33 @@ defmodule MicroserviceAppWeb.Plugs.RequestLogger do
 
   defp log_request(conn) do
     request_data = %{
-      request: %{
-        time: DateTime.utc_now() |> DateTime.to_iso8601(),
-        uri: "#{conn.scheme}://#{conn.host}:#{conn.port}#{conn.request_path}",
-        verb: conn.method,
-        headers: conn.req_headers |> Enum.into(%{}),
-        body: conn.body_params |> Jason.encode!()
-      }
+      time: DateTime.utc_now() |> DateTime.to_iso8601(),
+      uri: "#{conn.scheme}://#{conn.host}:#{conn.port}#{conn.request_path}",
+      verb: conn.method,
+      headers: conn.req_headers |> Enum.into(%{}),
+      body: conn.body_params |> Jason.encode!()
     }
-
     Logger.info("Request Data: #{Jason.encode!(request_data)}")
-    conn
+    assign(conn, :request_data, request_data)
   end
 
   defp log_response(conn) do
     response_data = %{
-      response: %{
-        time: DateTime.utc_now() |> DateTime.to_iso8601(),
-        status: conn.status,
-        headers: conn.resp_headers |> Enum.into(%{}),
-        body: conn.resp_body |> Jason.encode!()
-      },
-      transaction_id: UUID.uuid4(),
-      direction: "Incoming",
+      time: DateTime.utc_now() |> DateTime.to_iso8601(),
+      status: conn.status,
+      headers: conn.resp_headers |> Enum.into(%{}),
+      body: conn.resp_body |> Jason.encode!()
     }
 
-    response = post_to_remote(response_data)
+    combined_data = %{
+      request: conn.assigns[:request_data],
+      response: response_data,
+      transaction_id: UUID.uuid4(),
+      direction: "Incoming"
+    }
     Logger.info("Response: #{inspect(response)}")
 
+    RemoteLoggerQueue.enqueue(combined_data)
     conn
   end
 
